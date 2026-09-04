0a5967fefbb21c960e9e27e67268111eb10e1c3b
aamina-123 <aaminapathan73@gmail.com>
2026-09-04 15:46:40 +0530

    Add styling selection and generation functionality

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 function startStyling() {
     window.location.href = "styling.html";
 }
+let selectedStyle = {
+    occasion: "",
+    vibe: "",
+    colour: "",
+    season: ""
+};
+
+function selectOption(category, value, button) {
+    selectedStyle[category] = value;
+
+    const buttons = button.parentElement.querySelectorAll("button");
+
+    buttons.forEach(btn => {
+        btn.classList.remove("selected");
+    });
+
+    button.classList.add("selected");
+}
+
+function generateLook() {
+    if (
+        !selectedStyle.occasion ||
+        !selectedStyle.vibe ||
+        !selectedStyle.colour ||
+        !selectedStyle.season
+    ) {
+        alert("Please select an option from every section ✨");
+        return;
+    }
+
+    localStorage.setItem(
+        "auraStyle",
+        JSON.stringify(selectedStyle)
+    );
+
+    window.location.href = "result.html";
+}
